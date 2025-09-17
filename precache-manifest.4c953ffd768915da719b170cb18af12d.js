3e20cfc5fceffc4fdca7dc833626c1861eb96c61
sparshhgupta <sparsh105gupta@gmail.com>
2025-09-18 01:26:27 +0530

    Updates

---
+++ precache-manifest.4c953ffd768915da719b170cb18af12d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3107aad50d8644de8c823364ea1d00ec",
+    "url": "/trackvisiongui/index.html"
+  },
+  {
+    "revision": "f9fe9c8333eb1abe02f4",
+    "url": "/trackvisiongui/static/css/2.b71989ea.chunk.css"
+  },
+  {
+    "revision": "94114e862a6ce484653b",
+    "url": "/trackvisiongui/static/css/main.e962ad88.chunk.css"
+  },
+  {
+    "revision": "f9fe9c8333eb1abe02f4",
+    "url": "/trackvisiongui/static/js/2.18151bcb.chunk.js"
+  },
+  {
+    "revision": "1b669eb9dd0898f49cb9d66f9fc67749",
+    "url": "/trackvisiongui/static/js/2.18151bcb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7009dd71594faa678ec8",
+    "url": "/trackvisiongui/static/js/3.7820470f.chunk.js"
+  },
+  {
+    "revision": "94114e862a6ce484653b",
+    "url": "/trackvisiongui/static/js/main.0d457308.chunk.js"
+  },
+  {
+    "revision": "ed40b71496e6d5918172",
+    "url": "/trackvisiongui/static/js/runtime-main.d1db2d84.js"
+  }
+]);
